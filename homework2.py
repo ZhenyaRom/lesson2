79907c7824a840e7f5e711f288192ab25ecc0a14
ZhenyaRom <evladro2020@gmail.com>
2024-06-07 13:06:11 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+sum_of_homework = 12
+sum_of_time = 1.5
+name_of_course = 'Python'
+time_for_one_homework = sum_of_time/sum_of_homework
+print('Курс:', name_of_course, ', всего задач:', sum_of_homework, ', затрачено часов:', sum_of_time, ', среднее время выполнения', time_for_one_homework, 'часов.' )
